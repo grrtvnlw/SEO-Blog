6f031896bfe05c0a9cecf1c35624e80169e17260
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-07-08 11:57:24 -0400

    user sign up/in/out, authentication, adding JWT

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 // bring routes
 const blogRoutes = require("./routes/blog");
+const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -37,6 +38,7 @@ if (process.env.NODE_ENV === "development") {
 
 // routes middlewares
 app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
